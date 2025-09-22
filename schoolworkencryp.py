70b50cedf91e02862abb6b9d20d7dd2ddba8b954
Akki <93147274+2Akki@users.noreply.github.com>
2025-09-22 20:23:20 +0200

    Update schoolworkencryp.py
    update coments

--- schoolworkencryp.py
+++ schoolworkencryp.py
@@ -3,14 +3,18 @@ ALPHABET ="abcdefghijklmnopqrstuvwxyzæøå"
 
 L = len(ALPHABET)
 TO_NUM = {ch: i+1 for i, ch in enumerate(ALPHABET)}
+print(TO_NUM)
 TO_CHAR = {i+1: ch for i, ch in enumerate(ALPHABET)}
 TO_CHAR[0] = ""
-
+#Vi bruger enumerate til at give hvert bogstav et tal. 
+#Normalt starter det på 0, men vi lægger 1 til, så det starter på 1. TO_CHAR[0] = "" er for at undgå fejl.
 
 def decryptonize(id,cryptizedtext):
     uncryptizedtyext=[]
+    # slut uncryptizedtyext
     crypttextlower = cryptizedtext.lower().replace(" ","")
     idlower = id.lower().replace(" ","")
+    # vi sætter begge af dem til .lower().replace så vi både fjerne mellemrum og .lower så sortebogstaver ikke effecter kryptering 
     for i,(ch,p) in enumerate(zip(crypttextlower,idlower)):
 
         ct = TO_NUM[ch]
@@ -20,7 +24,7 @@ def decryptonize(id,cryptizedtext):
         print(løst)
         uncryptizedtyext.append(TO_CHAR[løst])
     return "".join(uncryptizedtyext)
-
+    # den returner den sidste løst uncryptizedtyext
 
 def cryptonize(id,text):
     cryptedtext=[]
@@ -40,10 +44,11 @@ def cryptonize(id,text):
 
     return "".join(cryptedtext)
 
-
-
+# det sammen basic som dekryptering men i stedet for et krypterede tekst kryptere vi teksten 
 ot = cryptonize("programmerin","sukkertoppen")
 print(ot)
 ot2=decryptonize("programmerin",ot)
 print(ot2)
+# her printes bare den dektryperede tekst og krypteret tekst :D
 
+#DER ER STAVE FEJL I TEKSTEN "uncryptizedtYext"
